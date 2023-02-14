069a15e74eb4b97c5a8bc47e50ca62f43ff44004
chrisgom113 <cgomez4@uci.edu>
2023-02-13 21:01:48 -0800

    fixed fetch error , passed album id info into correct button with fetch

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 //music match api key:    '2951db04af2babfa9b673302d170b3e1'
-function initPage () {
+function initPage() {
     var songEl = document.getElementById("left-container");
     var searchbtnEl = document.getElementById("search-btn");
     var inputEl = document.getElementById("lyric");
@@ -14,11 +14,11 @@ function initPage () {
     var clearBtn = document.getElementById("clear-history");
     var viewTracksBtn = document.getElementById("viewTracks");
     var searchHitEl = document.querySelectorAll("#searchHit");
-    
-    
+
+
 
     function getSong(lyricText) {
-    
+
         fetch(
             "https://stormy-hollows-86205.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=" +
             lyricText +
@@ -30,56 +30,62 @@ function initPage () {
             })
             .then(function (data) {
                 songEl.classList.remove("is-hidden");
-                var searchHitContainerEl = document.querySelectorAll("#searchHit");
-                for (i = 0; i < searchHitContainerEl.length; i++) {
-                    searchHitContainerEl[i].innerHTML = '';
-                    var tracksList = data.message.body.track_list;
-                    for (let i = 0; i < tracksList.length; i++) {
-                        const trackLoop = tracksList[i];
-                        var songTitle = trackLoop.track.track_name;
-                        var artistName = trackLoop.track.artist_name;
-                        var albumId = trackLoop.track.album_id;
-                        var albumName = trackLoop.track.album_name;
-                        var displaySongInfo = document.createElement("p");
-                        displaySongInfo.setAttribute(
-                            "class",
-                            "card-header-title"
-                        );
-                        displaySongInfo.innerHTML = ' "' + songTitle + '" by ' + artistName;
-                        displaySongInfo.addEventListener("click", function (event) {
-                            var selected = event.target.innerText;
-                            viewTracksBtn.setAttribute("data-id", albumId);
-                            modalContainerEl.classList.add('is-active');
-                            modalTitleEl.textContent = selected;
-                            displayGif(selected);
+                songEl.innerHTML = '';
 
-                            viewTracksBtn.addEventListener("click", function(event) {
-                                var album = event.target.getAttribute("data-id");
-                                fetch(
-                                    "https://stormy-hollows-86205.herokuapp.com/https://api.musixmatch.com/ws/1.1/album.tracks.get?album_id=" +
-                                    album +
-                                    "&page_size=10&apikey=" +
-                                    musicMatchAPIKey
-                                )
+                var tracksList = data.message.body.track_list;
+                for (let i = 0; i < tracksList.length; i++) {
+                    const trackLoop = tracksList[i];
+                    var songTitle = trackLoop.track.track_name;
+                    var artistName = trackLoop.track.artist_name;
+                    var albumId = trackLoop.track.album_id;
+                    console.log(albumId);
+                    var albumName = trackLoop.track.album_name;
+                    var displaySongHeader = document.createElement("header");
+                    displaySongHeader.classList.add("card-header");
+                    var displaySongInfo = document.createElement("p");
+
+                    displaySongInfo.setAttribute(
+                        "class",
+                        "card-header-title"
+                    );
+                    displaySongInfo.setAttribute("data-id", albumId);
+                    displaySongInfo.innerHTML = ' "' + songTitle + '" by ' + artistName;
+                    displaySongInfo.addEventListener("click", function (event) {
+                        var selected = event.target.innerText;
+                       var album = event.target.getAttribute("data-id");
+                        modalContainerEl.classList.add('is-active');
+                        modalTitleEl.textContent = selected;
+                        displayGif(selected);
+
+                        viewTracksBtn.addEventListener("click", function (event) {
+                            
+                            console.log(album);
+                            fetch(
+                                "https://stormy-hollows-86205.herokuapp.com/https://api.musixmatch.com/ws/1.1/album.tracks.get?album_id=" +
+                                album +
+                                "&page_size=10&apikey=" +
+                                musicMatchAPIKey
+                            )
                                 .then(function (response) {
                                     return response.json();
                                 })
                                 .then(function (data) {
                                     console.log(data);
                                 })
-                            })
-                            
-                        });
-                        searchHitContainerEl[i].append(displaySongInfo);
-                    }
+                        })
+
+                    });
+                    displaySongHeader.append(displaySongInfo);
+                    songEl.append(displaySongHeader);
                 }
+
             });
     }
-    
-    
-    
+
+
+
     function displayGif(selected) {
-        fetch('https://api.giphy.com/v1/gifs/search?api_key=nsaxiRBo6n5X0Io8kwVxtfJCkPtL0V2U&q=' + selected + '&limit=3&offset=0&rating=pg-13&lang=en')
+        fetch('https://api.giphy.com/v1/gifs/search?api_key=nsaxiRBo6n5X0Io8kwVxtfJCkPtL0V2U&q=' + selected + '&limit=20&offset=0&rating=pg-13&lang=en')
             .then(function (response) {
                 return response.json();
             })
@@ -94,7 +100,7 @@ function initPage () {
                 }
             })
     }
-    
+
     function renderSearchHistory() {
         historyEl.innerHTML = "";
         for (let i = 0; i < searchHistory.length; i++) {
@@ -112,19 +118,19 @@ function initPage () {
             historyEl.append(historyItem);
         }
     }
-    
+
     renderSearchHistory();
-    
+
     if (searchHistory.length > 0) {
         getSong(searchHistory[searchHistory.length - 1]);
     }
-    
+
     searchbtnEl.addEventListener("click", function () {
         for (let i = 0; i < searchHitEl.length; i++) {
             searchHitEl[i].innerHTML = '';
-            
+
         }
-       
+
         const lyricText = inputEl.value;
         getSong(lyricText);
         searchHistory.push(lyricText);
@@ -132,16 +138,16 @@ function initPage () {
         renderSearchHistory();
     });
 
-    
-    
+
+
     closeModalBtn.addEventListener('click', function () {
         modalContainerEl.classList.remove('is-active');
-    
+
     })
 
     closeModalBtn2.addEventListener('click', function () {
         modalContainerEl.classList.remove('is-active');
-    
+
     })
 
     clearBtn.addEventListener("click", function () {
