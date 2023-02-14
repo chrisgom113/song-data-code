eae701d4840990e6081346c2387c054589b5139d
chrisgom113 <cgomez4@uci.edu>
2023-02-13 20:11:20 -0800

    button glow, capitalize letters, margin, rounded

--- assets/script.js
+++ assets/script.js
@@ -101,7 +101,7 @@ function initPage () {
             const historyItem = document.createElement("input");
             historyItem.setAttribute("type", "text");
             historyItem.setAttribute("readonly", true);
-            historyItem.setAttribute("class", "form-control");
+            historyItem.setAttribute("class", "form-control  column has-text-centered my-2 button is-rounded");
             historyItem.setAttribute("value", searchHistory[i]);
             historyItem.addEventListener("click", function () {
                 for (let i = 0; i < searchHitEl.length; i++) {
